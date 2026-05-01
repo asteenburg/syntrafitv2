4149efbe82d25b233b732b95512dcd12fd5ee366
asteenburg <97249534+asteenburg@users.noreply.github.com>
2026-05-01 19:49:15 -0400

    Update SiteHeader.tsx

--- app/components/SiteHeader.tsx
+++ app/components/SiteHeader.tsx
@@ -141,7 +141,7 @@ export default function SiteHeader() {
                   <button
                     onClick={handleSignOut}
                     disabled={isSigningOut}
-                    className="w-full py-4 text-center font-bold text-[#e60000]  transition active:scale-[0.98]"
+                    className="w-full py-4 text-center font-bold text-[#e60000] transition active:scale-[0.98]"
                   >
                     {isSigningOut ? "Signing Out..." : "Sign Out"}
                   </button>
